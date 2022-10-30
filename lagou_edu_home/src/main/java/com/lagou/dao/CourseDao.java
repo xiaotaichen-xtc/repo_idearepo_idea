415fd301a4992c6842ac7a264117288913b00110
lagou_zimu <lagou_zimu@lagou.com>
2022-10-30 17:23:07 +0800

    修改 课程DAO

--- lagou_edu_home/src/main/java/com/lagou/dao/CourseDao.java
+++ lagou_edu_home/src/main/java/com/lagou/dao/CourseDao.java
@@ -28,4 +28,12 @@ public interface CourseDao {
 	//修改课程状态
 	int updateCourseStatus2(Course course);
 
+	public void test1();
+	public void test2();
+	public void test3();
+	public void test4();
+	public void test5();
+	public void test6();
+	public void test7();
+
 }
